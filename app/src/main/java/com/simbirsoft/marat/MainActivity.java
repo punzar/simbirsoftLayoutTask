2f221238bd88397ebaafa8594ca179b0a54574a1
punzar <punzar73@gmail.com>
2019-11-12 10:23:41 +0400

    prepare to merge

--- app/src/main/java/com/simbirsoft/marat/MainActivity.java
+++ app/src/main/java/com/simbirsoft/marat/MainActivity.java
@@ -1,13 +1,9 @@
 package com.simbirsoft.marat;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.Window;
-import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
--- app/src/main/java/com/simbirsoft/marat/ProfileActivity.java
+++ app/src/main/java/com/simbirsoft/marat/ProfileActivity.java
@@ -3,15 +3,9 @@ package com.simbirsoft.marat;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.content.Intent;
-import android.content.res.ColorStateList;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
