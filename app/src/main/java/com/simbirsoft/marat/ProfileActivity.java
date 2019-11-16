b7986ec64bddc1bc6124da5069cbd4a88bc7b356
punzar <punzar73@gmail.com>
2019-11-16 05:47:28 +0400

    unexpected behaviour

--- app/src/main/java/com/simbirsoft/marat/ProfileActivity.java
+++ app/src/main/java/com/simbirsoft/marat/ProfileActivity.java
@@ -1,15 +1,34 @@
 package com.simbirsoft.marat;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
-
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.squareup.picasso.Picasso;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ProfileActivity extends AppCompatActivity {
+    ImageView mProfileImage;
+    Dialog mChangePhotoDialog;
+    String mCurrentPhotoPath;
+    final static int IMAGE_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +42,45 @@ public class ProfileActivity extends AppCompatActivity {
         bottomNavigationView.setOnNavigationItemSelectedListener(getBottomNavigationListener());
 
         bottomNavigationView.setSelectedItemId(R.id.bottom_nav_profile);
+
+        mProfileImage = findViewById(R.id.profileImageView);
+        mProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mChangePhotoDialog = new Dialog(ProfileActivity.this);
+                mChangePhotoDialog.setContentView(R.layout.dialog_profile_set_photo);
+
+                TextView tvTakePhoto = mChangePhotoDialog.findViewById(R.id.dialogTvTakePhoto);
+                TextView tvDelete = mChangePhotoDialog.findViewById(R.id.dialogTvDelete);
+                ImageView ivTakePhoto = mChangePhotoDialog.findViewById(R.id.dialogIvTakePhoto);
+                ImageView ivDelete = mChangePhotoDialog.findViewById(R.id.dialogIvDelete);
+                dialogClickListener(tvTakePhoto);
+                dialogClickListener(tvDelete);
+
+                mChangePhotoDialog.show();
+
+            }
+        });
+    }
+
+    private void dialogClickListener(View view){
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()){
+                    case R.id.dialogTvTakePhoto:
+                        takePictureIntent();
+                        mChangePhotoDialog.cancel();
+                        break;
+                    case R.id.dialogTvDelete:
+                        mProfileImage.setImageResource(R.drawable.ic_help);
+                        mChangePhotoDialog.cancel();
+                        Toast.makeText(ProfileActivity.this, "Delete a photo from text", Toast.LENGTH_LONG).show();
+                        break;
+                }
+            }
+        });
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener getBottomNavigationListener() {
@@ -42,4 +100,45 @@ public class ProfileActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(imageFileName, ".jpg", storageDir);
+        mCurrentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void takePictureIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intent.resolveActivity(getPackageManager()) != null) {
+            File photoFile = null;
+
+            try {
+                photoFile = createImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this,"Some thing WRONG", Toast.LENGTH_LONG).show();
+            }
+
+            if(photoFile != null){
+                Uri photoUri = FileProvider.getUriForFile(this,"com.simbirsoft.marat.fileprovider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, IMAGE_REQUEST);
+            }
+
+        }
+    }
+
+    private void setmProfileImage(){
+        Picasso.get().load(new File(mCurrentPhotoPath)).fit().centerCrop().into(mProfileImage);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(IMAGE_REQUEST == requestCode && resultCode == RESULT_OK){
+            setmProfileImage();
+        }
+    }
 }
