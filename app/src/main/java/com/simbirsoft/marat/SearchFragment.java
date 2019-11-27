31ab04ecf855a807565f2f7dec489fbcea53b5b1
punzar <punzar73@gmail.com>
2019-11-27 16:56:28 +0400

    searchview questions

--- app/src/main/java/com/simbirsoft/marat/SearchFragment.java
+++ app/src/main/java/com/simbirsoft/marat/SearchFragment.java
@@ -22,7 +22,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.SearchView;
+import androidx.appcompat.widget.SearchView;
 
 import com.google.android.material.tabs.TabLayout;
 
